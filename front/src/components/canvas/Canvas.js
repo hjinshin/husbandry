6f949d9b6fca51c1c6eaf2b11867c89c81c34a74
hjinshin <gudwls818@gmail.com>
2023-11-11 04:58:23 +0900

    브라우저를 내렸을 때 위치 초기화

--- front/src/components/canvas/Canvas.js
+++ front/src/components/canvas/Canvas.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import draw from './draw';
 
 function Canvas(props) {
     const { animal, images, ...rest } = props;
+    const [isAnimationRunning, setIsAnimationRunning] = useState(true);
     const canvasRef = useRef(null);
     const startTimeRef = useRef(null);
 
@@ -16,7 +17,6 @@ function Canvas(props) {
         let x_direction = 1;
         let y_direction = -1;
         let angleCycle = 1;
-        let cnt = 0;
 
         function animate(timestamp) {
             if (!startTimeRef.current) {
@@ -35,7 +35,6 @@ function Canvas(props) {
             if(y > 640 - animal.height)            y_direction = -1; 
 
             x += (2 * progress) / 100 * x_direction; // 0.1초에 2 이동     
-
             if(true) {
                 // strolling
                 angle += (5 * progress) / 1000 * angleCycle;
@@ -45,19 +44,40 @@ function Canvas(props) {
             }
 
             if (progress >= 100) {
-                cnt ++;
                 startTimeRef.current = timestamp;
-            }
+            }                
+            
+
             draw(ctx, animal, images, x, y, x_direction, angle);
             animationId = requestAnimationFrame(animate);
         };
-
-        animationId = requestAnimationFrame(animate);
+        if (isAnimationRunning) {
+            animationId = requestAnimationFrame(animate);
+        }
         return () => {
             startTimeRef.current = null;
             cancelAnimationFrame(animationId);
           };
-    }, [animal, images]);
+    }, [animal, images, isAnimationRunning]);
+
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+          // 브라우저가 활성화되었을 때(isAnimationRunning이 true)만 스크롤 이벤트를 감지
+          if (document.visibilityState === 'visible') {
+            setIsAnimationRunning(true);
+          } else {
+            setIsAnimationRunning(false);
+          }
+        };
+    
+        // visibilitychange 이벤트 리스너 등록
+        document.addEventListener('visibilitychange', handleVisibilityChange);
+    
+        // 컴포넌트 언마운트 시 이벤트 리스너 제거
+        return () => {
+          document.removeEventListener('visibilitychange', handleVisibilityChange);
+        };
+      }, []);
 
     return (
     <div style={{position:'absolute'}}>    
